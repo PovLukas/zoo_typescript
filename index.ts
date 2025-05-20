c00ee617c67b622b0960c98672ed26e8235efb78
Lukas <lukpov@yahoo.com>
2025-05-20 16:36:28 +0300

    pradesiu formas

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+type CanDo = SleepAndWalk | JumpAndRun | SwimAndHunt;
+
+
+
+
 class Animal {
     name: string;
     age: number;
@@ -59,3 +64,86 @@ class Tiger extends Animal {
         this.wasOutsideIn8Hours = wasOutsideIn8Hours
     }
 }
+
+
+abstract class Employee {
+    enterZoo() {
+        console.log("Employee inside")
+    }
+    leaveZoo() {
+        console.log("Employee left")
+    }
+     takeSafetyTrainings():void {
+
+     }
+
+    isEmployeeAtZoo: boolean;
+    safetyTrainingCompletionDate: string;
+
+    constructor(isEmployeeAtZoo: boolean, safetyTrainingCompletionDate: string ) {
+        this.isEmployeeAtZoo = isEmployeeAtZoo;
+        this.safetyTrainingCompletionDate = safetyTrainingCompletionDate;
+    }
+}
+
+
+
+const whenFed: { animal: string, dateFed: Date }[] = [];
+
+class Zookeeper extends Employee {
+
+    feedTheAnimal(animal: string) {
+        const date = new Date()
+        console.log(`${animal} was fed ${date}`)
+        whenFed.push({animal, dateFed: date})
+    }
+
+    whenAnimalsWereFed() {
+        whenFed.forEach(e => {
+            console.log(`${e.animal} was fed ${e.dateFed}`)
+        })
+        }
+    }
+
+
+
+class Animals {
+     animalList: Animal[] = []; 
+
+
+    addAnimal(animal: Animal) {
+        this.animalList.push(animal);
+    }
+
+    listAnimals() {
+        this.animalList.forEach(animal => {
+            console.log(`Name: ${animal.name}, Age: ${animal.age}`);
+        });
+    }
+}
+
+    const zoo = new Animals()
+
+    const tiger = new Tiger(true, "Margis", 24)
+
+    zoo.addAnimal(tiger)
+
+    class Employees {
+        employeeList: Employee[] = [];
+
+            addEmployee(employee: Employee) {
+        this.employeeList.push(employee);
+    }
+
+    listEmployees() {
+        this.employeeList.forEach(employee => {
+            console.log(`Name: ${employee.isEmployeeAtZoo}, Age: ${employee.safetyTrainingCompletionDate}`);
+        });
+    }
+    }
+
+    const employeeList = new Employees()
+
+    const employee = new Zookeeper(true, "Balandzio 14")
+
+    employeeList.addEmployee(employee)
--- public/index.js
+++ public/index.js
@@ -27,3 +27,62 @@ class Tiger extends Animal {
         this.wasOutsideIn8Hours = wasOutsideIn8Hours;
     }
 }
+class Employee {
+    enterZoo() {
+        console.log("Employee inside");
+    }
+    leaveZoo() {
+        console.log("Employee left");
+    }
+    takeSafetyTrainings() {
+    }
+    constructor(isEmployeeAtZoo, safetyTrainingCompletionDate) {
+        this.isEmployeeAtZoo = isEmployeeAtZoo;
+        this.safetyTrainingCompletionDate = safetyTrainingCompletionDate;
+    }
+}
+const whenFed = [];
+class Zookeeper extends Employee {
+    feedTheAnimal(animal) {
+        const date = new Date();
+        console.log(`${animal} was fed ${date}`);
+        whenFed.push({ animal, dateFed: date });
+    }
+    whenAnimalsWereFed() {
+        whenFed.forEach(e => {
+            console.log(`${e.animal} was fed ${e.dateFed}`);
+        });
+    }
+}
+class Animals {
+    constructor() {
+        this.animalList = [];
+    }
+    addAnimal(animal) {
+        this.animalList.push(animal);
+    }
+    listAnimals() {
+        this.animalList.forEach(animal => {
+            console.log(`Name: ${animal.name}, Age: ${animal.age}`);
+        });
+    }
+}
+const zoo = new Animals();
+const tiger = new Tiger(true, "Margis", 24);
+zoo.addAnimal(tiger);
+class Employees {
+    constructor() {
+        this.employeeList = [];
+    }
+    addEmployee(employee) {
+        this.employeeList.push(employee);
+    }
+    listEmployees() {
+        this.employeeList.forEach(employee => {
+            console.log(`Name: ${employee.isEmployeeAtZoo}, Age: ${employee.safetyTrainingCompletionDate}`);
+        });
+    }
+}
+const employeeList = new Employees();
+const employee = new Zookeeper(true, "Balandzio 14");
+employeeList.addEmployee(employee);
